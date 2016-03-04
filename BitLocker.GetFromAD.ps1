a7f2197a5fe7926b1c3b294b68f43d68bdfdbf8c
KilgoreT <ki16or@gmail.com>
2016-03-04 17:29:14 +0500

    Список

--- BitLocker.GetFromAD.ps1
+++ BitLocker.GetFromAD.ps1
@@ -1,3 +1,4 @@
 ﻿#$computer = Get-ADComputer -Filter { OperatingSystem -NotLike '*server*' } -Property *
 #$BitLockerObjects = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -SearchBase [string]$computer.DistinguishedName -Properties 'msFVE-RecoveryPassword'
 $BitLockerObjects = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -Properties 'msFVE-RecoveryPassword'
+$BitLockerObjects | ft DistinguishedName
