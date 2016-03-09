f2548fd97a818445eb0aea38e9ef5faae46f3b96
KilgoreT <ki16or@gmail.com>
2016-03-09 16:59:43 +0500

    .

--- BitLocker.GetFromAD.ps1
+++ BitLocker.GetFromAD.ps1
@@ -1,7 +1,7 @@
 ﻿# Get BitLocker information from AD,
 #          such as: count bitlockered desktops, password recovery etc.
 
-
+#$regexp = 'CN.*'
 
 $BitLockerObjects = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -Properties 'msFVE-RecoveryPassword'
 $BitLockerObjects | ft DistinguishedName
