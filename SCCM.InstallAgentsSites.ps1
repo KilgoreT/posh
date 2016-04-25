652402c6e547a4fce000bdf42ecaf97dcd13b3b6
KilgoreT <ki16or@gmail.com>
2016-04-25 16:32:31 +0500

    InstallAgent
    Деплой агентов по сайтам

---
+++ SCCM.InstallAgentsSites.ps1
@@ -0,0 +1,21 @@
+﻿function Get-ComputerSite($ComputerName)
+{
+   $site = nltest /server:$ComputerName /dsgetsite 2>$null
+   if($LASTEXITCODE -eq 0){ $site[0] }
+}
+ 
+ $all_sites = @()
+ $all = Get-ADComputer -Filter * 
+
+ foreach ($comp in $all) {
+    $site = Get-ComputerSite $comp.Name
+   # Write-Host $site ": " $comp.Name
+    if ($site -eq "sh") {
+        Write-Host $site ": " $comp.Name
+        $all_sites += $comp.Name
+    }
+ }
+ 
+ 
+ 
+ Get-ComputerSite k1109012
