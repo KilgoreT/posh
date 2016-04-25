2342ec7fa189e9f67513e4e31c95e5e519911381
KilgoreT <ki16or@gmail.com>
2016-04-25 17:51:03 +0500

    ..

--- SCCM.InstallAgentsSites.ps1
+++ SCCM.InstallAgentsSites.ps1
@@ -1,11 +1,17 @@
-﻿function Get-ComputerSite($ComputerName)
+﻿# Function to get adsite of same pc.
+
+function Get-ComputerSite($ComputerName)
 {
    $site = nltest /server:$ComputerName /dsgetsite 2>$null
    if($LASTEXITCODE -eq 0){ $site[0] }
 }
  
+
+# Empty array to put pc from selected site.
  $all_sites = @()
+# All pc from AD
  $all = Get-ADComputer -Filter * 
+
 
  foreach ($comp in $all) {
     $site = Get-ComputerSite $comp.Name
@@ -13,6 +19,10 @@
     if ($site -eq "sh") {
         Write-Host $site ": " $comp.Name
         $all_sites += $comp.Name
+        $IsActive = Get-CMDevice -Name $comp.Name
+        if ($IsActive.IsActive -eq "True") {
+            Write-Host "Agent Already Installed!"
+        } else {Write-Host "Agent Not Installed."}
     }
  }
  
