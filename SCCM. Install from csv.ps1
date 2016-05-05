5e86881b36110de357c42c56cfa07fc5c3bf5501
KilgoreT <ki16or@gmail.com>
2016-05-05 10:12:09 +0500

    .

---
+++ SCCM. Install from csv.ps1
@@ -0,0 +1,21 @@
+﻿import-module ($Env:SMS_ADMIN_UI_PATH.Substring(0,$Env:SMS_ADMIN_UI_PATH.Length-5) + '\ConfigurationManager.psd1')
+$PSD = Get-PSDrive -PSProvider CMSite
+CD "$($PSD):"
+
+$pcs = Import-Csv -Path C:\temp\allsites.csv
+
+foreach ($i in $pcs) {
+
+    if ($i.CSite -eq "kln") {
+        $IsActive = Get-CMDevice -Name $i.CName
+        if ($IsActive.IsActive -eq "True") {
+                Write-Host $i.CName ": Agent Already Installed!"
+        } else {
+                Write-Host $i.CName ": Agent Not Installed."
+                # Get-CMDevice -Name $comp.Name | 
+                 Install-CMClient -DeviceName $i.CName -ForceReinstall $true -AlwaysInstallClient $true -SiteCode CCM
+                }
+                
+   }
+}
+ 
