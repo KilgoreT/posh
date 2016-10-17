eb72ae1995df4a08548aa8f7736dd4cd15fc11a1
KilgoreT <ki16or@gmail.com>
2016-10-17 17:59:43 +0500

    .

---
+++ SCCM. Select VM and NonVMs.ps1
@@ -0,0 +1,21 @@
+﻿import-module ($Env:SMS_ADMIN_UI_PATH.Substring(0,$Env:SMS_ADMIN_UI_PATH.Length-5) + '\ConfigurationManager.psd1')
+$PSD = Get-PSDrive -PSProvider CMSite
+CD "$($PSD):"
+
+$real = @()
+$unreal = @()
+
+$all = Get-Content C:\tmp\aida.txt
+
+foreach ($i in $all) {
+    $info = Get-CMDevice -Name $i
+    if ($info.IsVirtualMachine -eq $false) {
+        Write-Host $i " is real."-ForegroundColor Green
+        $real += $info.Name
+
+    } else {
+        Write-Host $i " is virtual." -ForegroundColor Red
+        $unreal += $info.Name
+    }
+}
+
