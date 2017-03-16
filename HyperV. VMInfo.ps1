84ddf094dc454d22c93fda0e6ffcc6dd69f588cc
KilgoreT <ki16or@gmail.com>
2017-03-16 16:01:16 +0500

    hyperVInfo
    2017.03.16

---
+++ HyperV. VMInfo.ps1
@@ -0,0 +1,33 @@
+﻿#Get-VM -ComputerName ulhost8 | ft ComputerName, Name, ProcessorCount, MemoryAssigned, State -AutoSize
+
+$AllVms = @()
+$Hosts = "Ulhost8", "Ulhost9", "Ulhost10", "Ulhost11", "Ulhost12", "Ulhost13", "Ulhost14", "suf2", "ulhost17", "nci-sql1", "nci-sql2"
+foreach ($h in $Hosts) {
+    $VMs = Get-VM -ComputerName $h
+    foreach ($vm in $VMs) {
+     
+        "host: " + $h + "; VM: " + $vm.Name 
+
+        $VmInfo = "" | Select "HostName", "VmName", "ProcessorCount", "MemoryAssigned", "HDD", "State"
+        $VmInfo.HostName = $h
+        $VmInfo.VmName = $vm.Name
+        $VmInfo.ProcessorCount = Get-VM -Name $vm.Name -ComputerName $h | Select-Object ProcessorCount
+        $VmInfo.MemoryAssigned = Get-VM -Name $vm.Name -ComputerName $h | Select-Object DynamicMemoryEnabled, MemoryAssigned, MemoryMinimum, MemoryMaximum
+        $VmInfo.HDD = Get-VM $vm.Name -ComputerName $h | Select-Object vmid | Get-VHD -ComputerName $h | Select-Object Size
+        $VmInfo.State = Get-VM -Name $vm.Name -ComputerName $h | Select-Object State
+
+        $AllVms += $VmInfo
+        $VmInfo = $null
+    }
+}
+
+$AllVms | Out-GridView
+
+#$VMs = Get-VM -ComputerName ulhost9
+
+#foreach ($i in $VMs) {
+#    $vmdisksize = Get-VM $i.Name -ComputerName ulhost9 | Select-Object vmid | Get-VHD -ComputerName ulhost9 | Select-Object Size
+#    #Get-VM -Name $i.Name -ComputerName ulhost9 | ft ComputerName, Name, ProcessorCount, MemoryAssigned, $vmdisksize, State -AutoSize
+#    Get-VM -Name $i.Name -ComputerName ulhost9 | Select-Object ComputerName, Name, ProcessorCount, MemoryAssigned, $vmdisksize, State
+    
+#}
